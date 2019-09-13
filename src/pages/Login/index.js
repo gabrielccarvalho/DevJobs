727fc79df71ae7df35d52707544f9b4ac417ef54
gabrielccarvalho <caampos22@gmail.com>
2019-09-13 07:46:48 -0300

    chore: add global styles

---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+export default function Login() {
+  return <Title>Login</Title>;
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
 import Main from './pages/Main';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/Login" component={Login} />
         <Route path="/" exact component={Main} />
       </Switch>
     </BrowserRouter>
