47228d4c382536250d3a2a136de1c50a4e41134f
gabrielccarvalho <caampos22@gmail.com>
2019-09-14 00:51:45 -0300

    chore: add login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Title } from './styles';
+import { Container } from './styles';
 
 export default function Login() {
-  return <Title>Login</Title>;
+  const [emailSelected, setEmailSelected] = useState(false);
+  const [email, setEmail] = useState('');
+  const [senhaSelected, setSenhaSelected] = useState(false);
+  const [senha, setSenha] = useState('');
+  return (
+    <>
+      <Container>
+        <form>
+          <br></br>
+          <div>
+            <img src="https://i.imgur.com/3ZMAPsP.png" />
+          </div>
+          <br></br>
+          <small>Log In to DevJobs</small>
+          <br></br>
+          <br></br>
+          <br></br>
+          <input
+            onFocus={() => {
+              setEmailSelected(true);
+            }}
+            onBlur={() => {
+              setEmailSelected(false);
+            }}
+            type="email"
+            placeholder={emailSelected ? '' : 'Email'}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            onFocus={() => {
+              setSenhaSelected(true);
+            }}
+            onBlur={() => {
+              setSenhaSelected(false);
+            }}
+            type="password"
+            placeholder={senhaSelected ? '' : 'Senha'}
+            onChange={e => setSenha(e.target.value)}
+          />
+          <button type="submit">Sign In</button>
+        </form>
+        <br></br>
+        <br></br>
+        <form className="register">
+          <small>New to DevJobs?</small> <a href="">Create an account</a>
+          <small>.</small>
+        </form>
+      </Container>
+    </>
+  );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,5 +1,91 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #333;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #ffffff;
+
+  form {
+    height: 45vh;
+    width: 45vh;
+    padding: 20px;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #ffffff;
+    -webkit-box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+    -moz-box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+    box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+  }
+
+  form.register {
+    height: 6vh;
+    width: 45vh;
+    padding: 20px;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #ffffff;
+    -webkit-box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+    -moz-box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+    box-shadow: -2px 10px 29px 0px rgba(0, 0, 0, 0.72);
+  }
+
+  a {
+    font-size: 16px;
+    font-weight: 300;
+    font-family: sans-serif;
+    text-decoration: none;
+    color: blue;
+  }
+
+  small {
+    font-size: 16px;
+    font-weight: 300;
+    font-family: sans-serif;
+  }
+
+  br {
+    margin-top: 2vh;
+  }
+
+  img {
+    width: 55px;
+    height: 55px;
+  }
+
+  input {
+    flex: 1;
+    height: 46px;
+    margin-bottom: 15px;
+    color: #777;
+    font-size: 15px;
+    width: 80%;
+    border: 0px;
+    outline: 0;
+    transition: all 0s ease-in-out;
+    border-bottom: 1px solid #c3cad9;
+  }
+
+  input:focus {
+    border-bottom: 2px solid #50d878;
+  }
+
+  button {
+    color: white;
+    font-size: 2vh;
+    font-weight: bold;
+    background: #50d878;
+    height: 5vh;
+    width: 80%;
+    border: 0;
+    border-radius: 5px;
+    margin-top: 25px;
+  }
 `;
